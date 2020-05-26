d59b245c3db596661692aeae92873aa3ce4d7dd6
VillaBen <2692560117@qq.com>
2020-05-26 14:38:51 +0800

    second commit to master

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTv1;
+    private Button mBtn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
